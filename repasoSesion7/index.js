85caca9c3b57a8c598aa4c88ed3afbf7e14a07e3
RobertCorreaT <ranireto@gmail.com>
2024-11-08 23:30:06 -0300

    Continuación persistencias de datos, CRUD Y POSTMAN: PUT.

--- repasoSesion7/index.js
+++ repasoSesion7/index.js
@@ -1,43 +1,97 @@
 import http from 'node:http';
-import { obtenerTareas } from './tareas.js';
+import {obtenerTareas, crearTarea, actualizarTareas} from './tareas.js';
+
 const PORT = 3001;
 // Creando el servidor
 http
     .createServer(async (req, res) => {
-    // const method = req.method;
-    // const url = req.url;
-    // Atrapando por destructuring
-    // const {method, url} = req;
-    // console.log(method, url);
 
-    try{
-    switch (req.method) {
-        case 'POST':
-            if(req.url === '/agregar' && req.method == 'POST'){
-                res.write('Vas a agregar nueva tarea');
-            }
-            break;
+        const {searchParams, pathname} = new URL(req.url, `https://${req.headers.host}`)
+        const params = new URLSearchParams(searchParams);
+        console.log(`Conexión exitosa.... pathname: ${pathname}`);
+        // const method = req.method;
+        // const url = req.url;
+        // Atrapando por destructuring
+        // const {method, url} = req;
+        // console.log(method, url);
+        const id = Number(req.url.split('/')[2]);
+        const rutaPut = Number(req.url.split('/'));
+        console.log(rutaPut)
+        console.log(id)
+        console.log(params)
 
-        case 'GET':
-            if(req.url === '/listar' && req.method == 'GET'){
-                res.write('Vas a listar las tareas');
-                const tareas = await obtenerTareas();
-                if(tareas.length > 0){
-                    res.end(JSON.stringify(tareas));
-                }else{
-                    res.end("Mensaje Se completó la operacion, pero la consulta esta vacía")
-                }
+        try {
+            switch (req.method) {
+                case 'POST':
+                    if (req.url == "/agregar") {
+                        // res.write('Vas a agregar nueva tarea');
+                        let body = '';
+                        req.on('data', (data) => {
+                            body += data.toString();
+                        })
+                        req.on('end', async () => {
+                            const nuevaTarea = JSON.parse(body);
+                            // console.log(nuevaTarea);
+                            const tareaCreada = await crearTarea(nuevaTarea.titulo);
+                            res.writeHead(201, {'Content-Type': 'application/json'});
+                            res.end(JSON.stringify(tareaCreada));
+                        })
+                    } else {
+                        res.writeHead(404, {'Content-Type': 'application/json'});
+                        res.end(404, {'mensaje:': 'Ruta no encontrada'})
+                    }
+                    break;
+
+                case 'GET':
+                    if (req.url === '/listar' && req.method == 'GET') {
+                        res.write('Vas a listar las tareas');
+                        const tareas = await obtenerTareas();
+                        if (tareas.length > 0) {
+                            res.end(JSON.stringify(tareas));
+                        } else {
+                            res.end("Mensaje Se completó la operacion, pero la consulta esta vacía")
+                        }
+                    }
+                    break;
+                case 'PUT':
+                    // if (req.url == "/actualizar") {
+
+                        // console.log(typeof (Number(req.url.split('/'))));
+                        if (!isNaN(id)) {
+                            let body = '';
+                            req.on('data', (data) => {
+                                body += data.toString();
+                            })
+                            req.on('end', async () => {
+                                const datosActualizados = JSON.parse(body);
+                                const tareaActualizada = await actualizarTareas(id, datosActualizados.completada)
+                                console.log(datosActualizados)
+
+
+                                if (tareaActualizada) {
+                                    res.writeHead(200, {'Content-Type': 'application/json'})
+                                    res.end(JSON.stringify(tareaActualizada));
+                                } else {
+                                    res.writeHead(404, {'Content-Type': 'application/json'});
+                                    res.end(JSON.stringify({'mensaje': 'Tarea Actualizada'}));
+                                }
+                            })
+                        // }
+                    } else {
+                        res.writeHead(404, 'Por favor indique un ID')
+                        res.end(JSON.stringify({'mensaje': 'Ruta no encontrada'}));
+                    }
+                    break;
+
+                default:
+                    res.end('No hay encontrado');
+                    break;
             }
-            break;
-        default:
-            res.end('No hay encontrado');
-            break;
-    }
-    }catch(e){
-        console.error('Errorrrr...');
-        res.end('Error interno del servidor');
-    }
-})
-.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-})
+        } catch (e) {
+            console.error('Errorrrr...');
+            res.end('Error interno del servidor');
+        }
+    })
+    .listen(PORT, () => {
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    })
--- repasoSesion7/tareas.js
+++ repasoSesion7/tareas.js
@@ -1,13 +1,70 @@
 import fs from 'fs/promises';
+
 const archivoTareas = 'tareas.json';
 
 export const obtenerTareas = async () => {
-    try{
-    //     Leer el archivo
+    try {
+        //     Leer el archivo
         const data = await fs.readFile(archivoTareas, 'utf8');
         return JSON.parse(data);
-    }catch (e){
+    } catch (e) {
         console.error('Error al leer el archivo');
-        return[];
+        return [];
+    }
+}
+
+async function guardarTareas(tareasGuardar) {
+    await fs.writeFile('./tareas.json', JSON.stringify(tareasGuardar, null, 2), 'utf8');
+}
+
+export const crearTarea = async (titulo) => {
+    const tareas = await obtenerTareas();
+
+    const nuevaTarea = {
+        id: Date.now(),
+        titulo: titulo,
+        completada: false
+    }
+    tareas.push(nuevaTarea);
+    await guardarTareas(tareas)
+    console.log('Tarea registrada exitosamente');
+    return nuevaTarea
+}
+
+// export const actualizarTareas = async (id, body) => {
+//     const tareas = await obtenerTareas();
+//
+//     const tarea = tareas.find(tarea => {
+//         return tarea.id === Number(id);
+//     })
+//
+//     if (tarea){
+//
+//     }
+// }
+
+//Función para actualizar las tareas
+export const actualizarTareas = async (id, terminada) => {
+    const tareas = await obtenerTareas();
+    //Como yo puedo buscar un valor dentro de un array - find()
+    // const tarea = tareas.find(tarea => {
+    //     return tarea.id === id;
+    // })
+    //Otra forma más actual de hacer la función
+    //console.log(typeof(id));
+
+    const tarea = tareas.find(tarea => {
+        //console.log(typeof(tarea.id));
+        //console.log('-----------------------');
+        return tarea.id === Number(id)
+    });
+    //console.log(tarea);
+    if (tarea) {
+        tarea.completada = new Boolean(terminada);
+        await guardarTareas(tareas);
+        console.log(`La tarea  ${id} se actualizo de manera satisfactoria!!!`);
+    } else {
+        console.log(`La tarea: ${id} no éxiste!!!`);
+        return null;
     }
 }
